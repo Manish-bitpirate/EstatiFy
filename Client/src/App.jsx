86e1b4ad44b3174e761f860e3db04775c07bc5cd
Manish <md663744@gmail.com>
2024-04-15 10:30:20 +0530

    header initial setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,9 +5,12 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
+import Header from './components/Header';
+
 export default function App() {
   return (
     <BrowserRouter >
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/sign-in" element={<SignIn/>} />
