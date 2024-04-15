8ff1c4a5ad168565f3513aea60355e1ee5fa513c
Manish <md663744@gmail.com>
2024-04-15 13:38:10 +0530

    initial db connection

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+
+//db
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+        console.log("Connected to db successfully");
+    }).catch((err)=>{
+        console.log(err);
+    });
+
+//server
 const app=express();
-
 app.get('/', (req, res)=> {
     res.send("Backend Response")
 });
-
 const listener=app.listen(8000, (req, res)=>{
     console.log("Server is running at port " + listener.address().port);
 });
