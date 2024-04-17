c528d4a6b906fa9e4ac0e2957f0729208b845f34
Manish <md663744@gmail.com>
2024-04-18 00:47:27 +0530

    created error handling middlewares

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcrypt";
+// import {errorHandler} from "../utils/error.js";
 
-export const signup=async(req, res)=>{
+export const signup=async(req, res, next)=>{
     const {username, email, password}=req.body;
     const hashedPass=bcryptjs.hashSync(password, 10);
     const newUser = new User({username, email, password:hashedPass});
@@ -11,6 +12,6 @@ export const signup=async(req, res)=>{
         res.status(201).send("User Created Successfully! 🎯");
     }
     catch(error) {
-        res.status(500).json(error.message);
+        next(error);
     }
 }
