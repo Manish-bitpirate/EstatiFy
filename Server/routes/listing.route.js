a981374b8abe11aa3de2f1ccb4ca866714678eff
Manish <md663744@gmail.com>
2024-05-28 12:17:46 +0530

    created update user listing api route

--- Server/routes/listing.route.js
+++ Server/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 //route to delete a listing
 router.delete("/delete/:id", verifyToken, deleteListing);
+//route to update a listing
+router.put("/update/:id", verifyToken, updateListing);
 
 export default router;
