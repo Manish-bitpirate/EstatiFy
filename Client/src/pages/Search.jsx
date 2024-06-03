187f55f78de0a7e151ba93ae674e2bc3c3b13765
Manish <md663744@gmail.com>
2024-06-03 15:07:42 +0530

    created listing item component

--- Client/src/pages/Search.jsx
+++ Client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 export default function Search() {
   const navigate = useNavigate();
@@ -15,8 +16,8 @@ export default function Search() {
     order: "desc",
   });
 
-  console.log(sidebarData);
-  console.log(listings);
+  // console.log(sidebarData);
+  console.log(listings, loading);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -48,16 +49,15 @@ export default function Search() {
       });
     }
 
-    const fetchListings = async() => {
+    const fetchListings = async () => {
       setLoading(true);
       const searchQuery = urlParams.toString();
       const res = await fetch(`/api/listing/get?${searchQuery}`);
       const data = await res.json();
       setListings(data);
       setLoading(false);
-    }
+    };
     fetchListings();
-
   }, [location.search]);
 
   const handleChange = (e) => {
@@ -216,10 +216,27 @@ export default function Search() {
           </button>
         </form>
       </div>
-      <div className="">
+
+      <div className="flex-1">
         <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
           Listing results:
         </h1>
+        <div className="p-7 flex flex-wrap gap-4">
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-slate-700">No listing found!</p>
+          )}
+          {loading && (
+            <p className="text-xl text-slate-700 text-center w-full">
+              Loading...
+            </p>
+          )}
+
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+        </div>
       </div>
     </div>
   );
