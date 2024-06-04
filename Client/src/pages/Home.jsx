4a4b65ffc604ef6fc937fbf1022c554662950df3
Manish <md663744@gmail.com>
2024-06-04 19:04:01 +0530

    fixed the sale listing not showing error

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -36,7 +36,7 @@ export default function Home() {
 
     const fetchSaleListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?type=sale&limit=4');
+        const res = await fetch('/api/listing/get?type=sell&limit=4');
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
@@ -116,7 +116,7 @@ export default function Home() {
           <div className=''>
             <div className='my-3'>
               <h2 className='text-2xl font-semibold text-slate-600'>Recent places for sale</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=sale'}>Show more places for sale</Link>
+              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=sell'}>Show more places for sale</Link>
             </div>
             <div className='flex flex-wrap gap-4'>
               {saleListings.map((listing) => (
