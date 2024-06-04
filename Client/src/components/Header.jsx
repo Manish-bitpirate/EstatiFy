d8630be3af777e415798f06ac4f80aa40a120d3c
Manish <md663744@gmail.com>
2024-06-04 13:48:40 +0530

    completed home and about page

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -30,8 +30,8 @@ useEffect(() => {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-400 ">Real</span>
-            <span className="text-slate-700 ">Estate</span>
+            <span className="text-slate-400 ">Estat</span>
+            <span className="text-slate-700 ">iFy</span>
           </h1>
         </Link>
         <form
