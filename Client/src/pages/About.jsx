5cc4729fc68abc09bbec5b024d2144575f34ef33
Manish <md663744@gmail.com>
2024-06-09 10:58:33 +0530

    minor changes

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <p className='mb-4 text-slate-700'>
       Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
       </p>
-      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience above all, and we are dedicated to making that a reality for each and every one of our clients.</p>
     </div>
   )
 }
